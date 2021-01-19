888e9f7ffacf9988ef5525cb6abdf66ef7c42a9a
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-19 20:44:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  index = 0 
+  lowest_key = nil 
  name_hash.each do |key, value|
-   if value[index] < lowest_value 
-     value[index] = lowest_value 
-    key[index] == lowest_key
-   end
-   index += 1 
- end
- end 
+   if 
+     
+end
