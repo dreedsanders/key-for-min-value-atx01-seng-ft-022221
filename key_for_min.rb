a2e4b1a9c7214a9fc85469180ab0562655ca89b6
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-19 21:05:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,14 @@
 def key_for_min_value(name_hash)
   index = 0 
   lowest_key = nil
-  lowest_value = value[index]
+  lowest_value = nil
  name_hash.each do |key, value|
    if
+     lowest_value = nil 
+     lowest_value = value 
+     elsif
      value < lowest_value
-     lowest_value = value
-     lowest_key = key 
+     value = lowest_value
    end
  end
  lowest_key
