cd1493cc22dd8a6ad47bee75e9b2a409c2b05402
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-19 21:09:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,8 +11,9 @@ def key_for_min_value(name_hash)
      lowest_value = value 
      elsif
      value < lowest_value
-     value = lowest_value
+     lowest_value = value 
+     lowest_key = key 
    end
  end
- lowest_key
+ puts lowest_key
 end
